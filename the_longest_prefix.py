4525411ff26119724d7eb84004c8c579dc63d22a
x.bayramov <Xabibulloh.Bayramov@anorbank.uz>
2025-12-09 20:08:45 +0500

    the longest prefix done with v2

--- the_longest_prefix.py
+++ the_longest_prefix.py
@@ -7,7 +7,20 @@ def longestCommonPrefix(words: list) -> str:
             if short[i] != word[i]:
                 return prefix
         prefix = short[:i+1]
-    return short
+    return ""
+
+print(longestCommonPrefix(["flower","flow","flight"]))
+
+def longestCommonPrefixV2(words: list) -> str:
+    short = min(words, key=len)
+    m = len(short)
+    prefix = ""
+    for i in range(m):
+        for word in words:
+            if short[i] != word[i]:
+                return word[:prefix]
+        prefix = i+1
+    return ""
 
 
 print(longestCommonPrefix(["flower","flow","flight"]))
